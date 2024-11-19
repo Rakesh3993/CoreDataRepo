7394513d9304963e79a7014e87e84381ef2b2e1a
Rakesh3993 <kr.rakesh3993@gmail.com>
2024-11-19 19:17:23 +0530

    Core Data

---
+++ CoreDataDemo/Model/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreDataDemo
+//
+//  Created by Rakesh Kumar on 19/11/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
